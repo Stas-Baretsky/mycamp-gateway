5a6445524eb8239842345b5b940e2c895499d55a
PajiloyMangust <podoprigoravadim@rambler.ru>
2026-07-23 19:03:22 +0500

    fixes

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,6 +1,9 @@
 package app
 
 import (
+	"context"
+	"gateway-api/internal/bootstrap"
+	"gateway-api/internal/config"
 	"gateway-api/internal/middleware/ratelimiter"
 	"log/slog"
 	"net/http"
@@ -12,10 +15,16 @@ type Application struct {
 	Limiter *ratelimiter.RateLimiter
 }
 
-func (a *Application) MustRun() {
-	///
+func (a *Application) MustRun(ctx context.Context, cfg config.Config) {
+	app, err := bootstrap.Build(ctx, cfg)
+	if err != nil {
+		panic(err)
+	}
+	return &app
 }
 
 func (a *Application) Stop() {
-	///
+	///server stop
+	///logger stop
+	///limiter stop
 }
--- internal/bootstrap/bootstrap.go
+++ internal/bootstrap/bootstrap.go
@@ -2,6 +2,8 @@ package bootstrap
 
 import (
 	"context"
+	"fmt"
+
 	"gateway-api/internal/app"
 	limiterCache "gateway-api/internal/cache/limiterCache"
 	"gateway-api/internal/config"
@@ -18,15 +20,20 @@ const (
 )
 
 func Build(ctx context.Context, cfg config.Config) (*app.Application, error) {
+	const op = "bootstrap.bootstrap.Build"
+
 	log := setupLogger(cfg.Env)
 	log.Info("Starting service...", slog.String("env", cfg.Env))
 
 	redisStorage, err := limiterCache.NewClient(ctx, cfg)
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
 	limiter, err := ratelimiter.NewRateLimiter(ctx, log, redisStorage, &cfg.RateLimiter)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
 
 	return &app.Application{
 		Server:  &http.Server{},
